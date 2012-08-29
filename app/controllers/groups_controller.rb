a9bcb71328e83354562ac99e633d0c635e88e03e
Richard King <rkingucla@ymail.com>
2012-08-28 19:48:54 -0700

    change to leagues and start weekly functionality

--- app/controllers/groups_controller.rb
+++
@@ -1,7 +0,0 @@
-class GroupsController < ApplicationController
-
-  def show
-    @current_user_teams = current_user.find_teams(params[:id])
-    @all_teams_names = Team.all.each.map{|t| "#{t.city} #{t.name}"}
-  end
-end

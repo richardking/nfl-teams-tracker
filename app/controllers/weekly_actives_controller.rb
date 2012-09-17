c9d203671b55e5eee0c5e6f0f02ee0761befb279
Richard King <rkingucla@ymail.com>
2012-09-16 17:22:17 -0700

    fix usersleague entry in weeklyactive table

--- app/controllers/weekly_actives_controller.rb
+++ app/controllers/weekly_actives_controller.rb
@@ -7,7 +7,7 @@ class WeeklyActivesController < ApplicationController
     end
     if WeeklyActive.find_all_by_user_id_and_league_id_and_week_id(params[:user_id], params[:league_id], params[:id]).count < 6
       users_league_id = UsersLeague.find_by_user_id_and_league_id(params[:user_id], params[:league_id])
-      WeeklyActive.create(league_id: params[:league_id], user_id: params[:user_id], week_id: params[:id], pick_id: params[:pick_id], users_league_id: users_league_id)
+      WeeklyActive.create(league_id: params[:league_id], user_id: params[:user_id], week_id: params[:id], pick_id: params[:pick_id], users_league_id: users_league_id.id)
       redirect_to request.referrer, flash: { success: "Success!"}
     else
       redirect_to request.referrer, alert: "You already have 6 teams starting"
